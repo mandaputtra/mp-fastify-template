8a112cd4108cecfe959c5e9c005a21a3a3f8f21a
mandaputtra <mandaputra8@gmail.com>
2018-11-20 13:42:35 +0800

    using fastify

--- server/src/services/index.js
+++
@@ -1,9 +0,0 @@
-const users = require('./users/users.service.js');
-const roles = require('./roles/roles.service.js');
-const menus = require('./menus/menus.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(users);
-  app.configure(roles);
-  app.configure(menus);
-};
