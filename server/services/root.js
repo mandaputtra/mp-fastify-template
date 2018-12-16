b5553255d86b82412e1cd7fa0e9e24005dc67c8e
mandaputtra <mandaputra8@gmail.com>
2018-12-16 15:12:16 +0800

    try fastify in deepth

--- server/services/root.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// If you prefer async/await, use the following
-
-module.exports = async function(fastify, opts) {
-  fastify.get("/", async function(request, reply) {
-    reply.code(200).send({ message: "API ready!" });
-  });
-};
